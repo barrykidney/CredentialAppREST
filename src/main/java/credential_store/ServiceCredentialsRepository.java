8164c6f5b13214f8fc3c0d4d8c4976bf46176cb5
barrykidney <barrykidney@hotmail.com>
2019-12-19 22:30:46 +0000

    refactor to return a list of reduced credentialDTO object for a get all request
    Signed-off-by: barrykidney <barrykidney@hotmail.com>

--- src/main/java/credential_store/ServiceCredentialsRepository.java
+++ src/main/java/credential_store/ServiceCredentialsRepository.java
@@ -1,8 +1,11 @@
 package credential_store;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import credential_store.ServiceCredentials;
+
+import java.util.Collection;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
